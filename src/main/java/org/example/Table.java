7123f6380ca135ac856e71b04b19ea9130c7c4a5
miiiiku <w.m124@icloud.com>
2024-06-06 00:42:49 +0900

    second commit

---
+++ src/main/java/org/example/Table.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Table {
+
+    // TODO add raizing
+
+    public void setI(int j) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Count: " + i);
+        }
+        System.out.println("Hello, world!");
+    }
+
+    //TODO komier
+
+
+}
