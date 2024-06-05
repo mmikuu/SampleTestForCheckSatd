8e0c48e840d2a4db1811301b88e6002c0fb0fbb2
miiiiku <w.m124@icloud.com>
2024-06-06 00:47:42 +0900

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        //TODO add def
+        //TODO def
+
         System.out.println("Hello world!");
     }
 }
