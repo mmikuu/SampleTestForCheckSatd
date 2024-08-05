ec5cc82e7cbbd3cd37bbcf44dd007787c41c7847
miiiiku <w.m124@icloud.com>
2024-08-05 13:06:20 +0900

    branch切ってみた

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-
+        System.out.println("konnitiwa");
+        for(int i = 0; i<6; i++){
+            //TODO change name
+            int x = i;
+            System.out.println(x);
+        }
         System.out.println("Hello world!");
         //FIX this u
 
